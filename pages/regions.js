66e1604911ab852155213dd2eb6a4ac4e063099d
ShinySylveon04 <carlotta.basketball04@gmail.com>
2019-03-01 12:44:01 -0600

    Still fixing gh-pages

--- pages/regions.js
+++ pages/regions.js
@@ -44,7 +44,7 @@ export default () => {
           }}
         >
           <h1>Pokemon Regions</h1>
-          <Link href="/">
+          <Link href="/" as={process.env.BACKEND_URL + "/"}>
             <a>#Pokemon Facts</a>
           </Link>
         </div>
